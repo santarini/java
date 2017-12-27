bcf90d02a23dc74b63253673d290f42224d7fd2e
Makoa <32736507+santarini@users.noreply.github.com>
2017-12-26 20:33:24 -1000

    Create FutureValueCalc.java

---
+++ RedditDaily/Easy/FutureValueCalc.java
@@ -0,0 +1,31 @@
+package com.example.java;
+
+import java.util.Scanner;
+
+public class FutureValueCalc {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("How much money will you start with?");
+        int pv = reader.nextInt();
+        System.out.println("What percentage interest rate can you achieve per year? ");
+        float pr = reader.nextInt();
+        float i = (pr/100);
+        System.out.println("How many years do you plan to maintain the position? ");
+        int n = reader.nextInt();
+        float EAR = (float)Math.pow((1+i),n);
+        System.out.println("Are you planning on making regular contributions or withdrawals from the inital deposit? ");
+        String pmtQ = reader.next();
+
+        //Test Shit beneath here
+        System.out.println("Your PV " + pv + ", your pr " + pr + ", your i " + i +", your n " + n +" EAR " + EAR);
+        float pmt;
+        if (pmtQ.equals("Yes")){
+            System.out.println("How much are you planning to withdraw or deposit (net contribution)?");
+            pmt = reader.nextInt();
+        }else pmt = 0;
+        float fvPMT = (pmt *(EAR - 1)/i);
+        float fv = ((pv * EAR) + fvPMT);
+        System.out.println(fv);
+    }
+}
