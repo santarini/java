ecd213cdd67bd2da7e69714cbb99733062e6c883
Makoa <32736507+santarini@users.noreply.github.com>
2017-12-30 12:29:19 -1000

    Update BottlesOfBeer.java

--- RedditDaily/Easy/BottlesOfBeer.java
+++ RedditDaily/Easy/BottlesOfBeer.java
@@ -2,16 +2,15 @@ package com.example.java;
 
 import java.util.Scanner;
 
-public class BottleOfBeer {
+public class Main {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.println("How much many bottle of beer?");
         int start = reader.nextInt();
         int lessStart = start - 1;
-        for (int i = start; i >= 1; start--){
-            System.out.println(start + " Bottles of beer on the wall, " + start + " bottles of beer. \n" + "Take one down and pass it around," + lessStart + " bottles of beer on the wall." );
+        for (int i = start; i >= 1; i-= 1){
+            System.out.println(i + " Bottles of beer on the wall, " + i + " bottles of beer. \n" + "Take one down and pass it around," + (i - 1) + " bottles of beer on the wall." );
         }
     }
 }
-//At the moment this runs infinitely
