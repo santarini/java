0bad9fd7d87677db3f2794375dcfe3020c147a83
Makoa <32736507+santarini@users.noreply.github.com>
2017-12-30 18:49:32 -1000

    Update and rename BottlesOfBeer.java to _08_BottlesOfBeer.java

--- RedditDaily/Easy/_08_BottlesOfBeer.java
+++ RedditDaily/Easy/_08_BottlesOfBeer.java
@@ -2,7 +2,7 @@ package com.example.java;
 
 import java.util.Scanner;
 
-public class Main {
+public class _08_BottlesOfBeer {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
